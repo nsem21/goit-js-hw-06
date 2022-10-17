1671799b739d3c121a812641b27210cd896f0190
Nadiia Semkiv <48651806+nsem21@users.noreply.github.com>
2022-10-17 10:53:23 +0300

    update task no.6

--- js/task-06.js
+++ js/task-06.js
@@ -1,17 +1,29 @@
 const userInput = document.querySelector('#validation-input');
 const checkLength = Number(userInput.getAttribute('data-length'));
 
+function toChangeBorder(validationCode) {
+    if (validationCode === 'valid') {
+        userInput.classList.remove('invalid');
+        userInput.classList.add('valid');
+        return;
+    } else if (validationCode === 'invalid') {
+        userInput.classList.remove('valid');
+        userInput.classList.add('invalid');
+        return;
+    } else {
+        userInput.classList.remove('valid');
+        userInput.classList.remove('invalid');
+    }
+}
+
 const toValidate = (newEvent) => {
     const measurement = newEvent.currentTarget.value.length;
     if (measurement === checkLength) {
-        //userInput.classList.remove('valid')
-        userInput.classList.remove('invalid')
-        userInput.classList.add('valid')
+        toChangeBorder('valid');
 
     } else {
-        userInput.classList.remove('valid')
-            //userInput.classList.remove('invalid')
-        userInput.classList.add('invalid')
+        toChangeBorder('invalid');
     }
 }
-userInput.addEventListener('change', toValidate);
+userInput.addEventListener('focus', toChangeBorder);
+userInput.addEventListener('blur', toValidate);
